c77bc0ccd3cc58384e679bfeb284ee19b36c3af3
gordon_bates <gordonbatesofficial@gmail.com>
2022-08-31 19:33:23 +0000

    Add open and close animation functions for the hamburger menu in index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// navbar
+function hideIconBar(){
+    var iconBar = document.getElementById("iconBar");
+    var navigation = document.getElementById("navigation");
+    iconBar.setAttribute("style", "display:none;");
+    navigation.classList.remove("hide-nav");
+}
+
+function showIconBar(){
+    var iconBar = document.getElementById("iconBar");
+    var navigation = document.getElementById("navigation");
+    iconBar.setAttribute("style", "display:block;");
+    navigation.classList.add("hide-nav");
+}
